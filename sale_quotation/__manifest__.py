b445bc468719d013308f96027af435db54135cf5
omarradwan33 <omarradwan396@gmail.com>
2025-12-25 01:14:28 +0200

    [ADD] sale order module

---
+++ sale_quotation/__manifest__.py
@@ -0,0 +1,19 @@
+# pylint: skip-file
+{
+    "name": "Sale Order Status",
+    "summary": "",
+    "author": "",
+    "website": "",
+    "version": "1.0.0",
+    "category": "",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/sale_order_quotation_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
