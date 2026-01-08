0c9a00d3bcd8b4de8eafd87dc2d14213c6103488
omarradwan33 <omarradwan396@gmail.com>
2026-01-09 00:09:11 +0200

    [ADD]new module frtz and purchase order

---
+++ purchase_order/__manifest__.py
@@ -0,0 +1,19 @@
+# pylint: skip-file
+{
+    "name": "Purchase Order Status",
+    "summary": "",
+    "author": "",
+    "website": "",
+    "version": "1.0.0",
+    "category": "",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "views/purchase_order_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
