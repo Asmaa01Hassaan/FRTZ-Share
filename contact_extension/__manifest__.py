66fcfe7cf0e7cd2dcb55c3b079ff5447f44bb25f
omarradwan33 <omarradwan396@gmail.com>
2026-01-13 00:09:39 +0200

    [ADD] custom module to modify contact

---
+++ contact_extension/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Contact Extension",
+    "version": "18.0.1.0.0",
+    "summary": "",
+    "license": "LGPL-3",
+    "author": "Omar Radwan",
+    "depends": ['base'],
+    "data": [
+        "views/contact_base_info_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+
+}
