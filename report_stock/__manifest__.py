efbdd072688b258f052ba509bc468714a9b91809
omarradwan33 <omarradwan396@gmail.com>
2026-02-05 04:47:22 +0200

    [ADD] new report stock

---
+++ report_stock/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Report Stock",
+    "version": "18.0.1.0.0",
+    "summary": "",
+    "license": "LGPL-3",
+    "author": "Omar Radwan",
+    "depends": ['stock'],
+    "data": [
+        "report/inventory_report.xml",
+    ],
+    "application": False,
+    "installable": True,
+
+}
